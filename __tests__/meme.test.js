9e6c2fee0524590caf30913c555e78fb221a50f2
Nikki <nikki.satopdx@gmail.com>
2020-02-17 15:48:05 -0800

    all tests are passing

--- __tests__/meme.test.js
+++ __tests__/meme.test.js
@@ -15,15 +15,15 @@ describe('event routes', () => {
     return mongoose.connection.dropDatabase();
   });
 
-  //   let meme;
-  //   beforeEach(async () => {
-  //     meme = await Meme.create({
-  //       headerText: 'HI',
-  //       imageUrl:
-  //         'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/25201637/day_2_dec_14_085.jpg',
-  //       footerText: 'Corgi'
-  //     });
-  //   });
+  let meme;
+  beforeEach(async () => {
+    meme = await Meme.create({
+      headerText: 'HI',
+      imageUrl:
+        'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/25201637/day_2_dec_14_085.jpg',
+      footerText: 'Corgi'
+    });
+  });
 
   afterAll(() => {
     return mongoose.connection.close();
@@ -42,12 +42,69 @@ describe('event routes', () => {
         console.log(res.body);
         expect(res.body).toEqual({
           _id: expect.any(String),
-          headerText: 'HI',
-          imageUrl:
-            'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/25201637/day_2_dec_14_085.jpg',
-          footerText: 'Corgi',
+          headerText: expect.any(String),
+          imageUrl: expect.any(String),
+          footerText: expect.any(String),
           __v: 0
+        });
+      });
+  });
 
+  it('it gets all memes', () => {
+    return request(app)
+      .get('/api/v1/memes')
+      .then(res => {
+        expect(res.body).toEqual([
+          {
+            _id: expect.any(String),
+            headerText: expect.any(String),
+            imageUrl: expect.any(String),
+            footerText: expect.any(String),
+            __v: 0
+          }
+        ]);
+      });
+  });
+
+  it('gets a meme by Id', () => {
+    return request(app)
+      .get(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          headerText: expect.any(String),
+          imageUrl: expect.any(String),
+          footerText: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  it('updates a meme by Id', () => {
+    return request(app)
+      .patch(`/api/v1/memes/${meme._id}`)
+      .send({ footerText: 'HI' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          headerText: expect.any(String),
+          imageUrl: expect.any(String),
+          footerText: 'HI',
+          __v: 0
+        });
+      });
+  });
+
+  it('deletes a meme by Id', () => {
+    return request(app)
+      .delete(`/api/v1/memes/${meme._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          headerText: expect.any(String),
+          imageUrl: expect.any(String),
+          footerText: expect.any(String),
+          __v: 0
         });
       });
   });
